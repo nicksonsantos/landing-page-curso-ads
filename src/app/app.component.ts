48d1bf8ca7ddd386e18c4550c624e9257f576d95
nicksonendlich <nickson.endlich@autoglass.com.br>
2024-06-22 13:46:49 -0300

    Landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'landing-page-curso-ads';
+export class AppComponent implements OnInit {
+  formRegistro: FormGroup = this.fb.group({
+    nome: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    telefone: ['', Validators.required],
+    mensagem: ['', Validators.required]
+  });;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  onSubmit() {
+    if (this.formRegistro.valid) {
+      // Lógica para processar os dados do formulário
+      console.log(this.formRegistro.value);
+    }
+  }
 }
